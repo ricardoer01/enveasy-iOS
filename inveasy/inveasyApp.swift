9b66a668f0bcc9bd61ffb50c00c60da0b9a19330
ricardoer <ricardeuan@gmail.com>
2026-06-07 16:52:25 -0600

    Initial Commit

---
+++ inveasy/inveasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  inveasyApp.swift
+//  inveasy
+//
+//  Created by Ricardo ER on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct inveasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
